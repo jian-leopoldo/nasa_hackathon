203a94011148da7b67540f341ba91e293446cfb5
Jian <jian.leopoldo@gmail.com>
2018-10-20 04:30:49 -0300

    SDK do google vision adicionado

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,2 +1,16 @@
 class Challenge < ApplicationRecord
+  require "google/cloud/vision"
+
+  def self.send_image
+    vision = Google::Cloud::Vision.new
+
+    image = vision.image "./public/george.jpg"
+
+    annotation = image.annonate
+
+    byebug
+
+    landmark.description
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+require "google/cloud/vision"
+
 
 module HackathonNasa
   class Application < Rails::Application
@@ -15,5 +17,13 @@ module HackathonNasa
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    Google::Cloud::Vision.configure do |config|
+      config.project_id  = "hackathon-nasa"
+      config.credentials = "./key-gcloud-vision.json"
+    end
+
+    vision = Google::Cloud::Vision.new
+
   end
 end
